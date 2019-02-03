877ea562634e96f81790c1a3f6cdbdd49af55d80
skywind3000 <skywind3000@163.com>
2019-02-04 05:10:07 +0800

    fzf on cygwin

--- z.lua
+++ z.lua
@@ -1393,7 +1393,10 @@ function z_cd(patterns)
 		local cmd = ((not fzf) and 'fzf' or fzf)  .. ' ' .. cmd
 		if not windows then
 			tmpname = os.tmpname()
-			cmd = 'cat "' .. tmpname .. '" | ' .. cmd .. ' --height 35%'
+			cmd = 'cat "' .. tmpname .. '" | ' .. cmd 
+			if not os.getenv('_ZL_FZF_FULLSCR') then
+				cmd = cmd .. ' --height 35%'
+			end
 		else
 			tmpname = os.tmpname():gsub('\\', ''):gsub('%.', '')
 			tmpname = os.getenv('TMP') .. '\\zlua_' .. tmpname .. '.txt'
