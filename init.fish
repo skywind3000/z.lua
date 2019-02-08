33fe8ac7d0deda11f1acd6a8903d07937f30e8cd
skywind3000 <skywind3000@163.com>
2019-02-08 12:13:45 +0800

    commit init.fish

--- init.fish
+++ init.fish
@@ -18,24 +18,25 @@ end
 
 set -x _ZL_DATA "$_ZL_DATA"
 
-# set _zlua_dir (dirname (status --current-filename))
+set _zlua_dir (dirname (status --current-filename))
 
-if type -q lua
-	lua $path/z.lua --init fish enhanced once echo | source
-else if type -q lua5.3
-	lua5.3 $path/z.lua --init fish enhanced once echo | source
-else if type -q lua5.2
-	lua5.2 $path/z.lua --init fish enhanced once echo | source
-else if type -q lua5.1
-	lua5.1 $path/z.lua --init fish enhanced once echo | source
-else
-	echo "init z.lua failed, not find lua in your system"
+if test -e $_zlua_dir/z.lua 
+	if type -q lua
+		lua $_zlua_dir/z.lua --init fish enhanced once echo | source
+	else if type -q lua5.3
+		lua5.3 $_zlua_dir/z.lua --init fish enhanced once echo | source
+	else if type -q lua5.2
+		lua5.2 $_zlua_dir/z.lua --init fish enhanced once echo | source
+	else if type -q lua5.1
+		lua5.1 $_zlua_dir/z.lua --init fish enhanced once echo | source
+	else
+		echo "init z.lua failed, not find lua in your system"
+	end
+	alias zc='z -c'      # restrict matches to subdirs of $PWD
+	alias zz='z -i'      # cd with interactive selection
+	alias zf='z -I'      # use fzf to select in multiple matches
+	alias zb='z -b'      # quickly cd to the parent directory
 end
 
-
-alias zc='z -c'      # restrict matches to subdirs of $PWD
-alias zz='z -i'      # cd with interactive selection
-alias zf='z -I'      # use fzf to select in multiple matches
-alias zb='z -b'      # quickly cd to the parent directory
 
 
